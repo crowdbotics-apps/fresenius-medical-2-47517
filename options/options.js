eeb4dc2b807035456ada949f87a5a56ce985a3ea
Crowdbotics <team@crowdbotics.com>
2024-04-15 18:42:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fresenius-medical-2-47517",
+  url: "https://fresenius-medical-2-47517.botics.co",
+  api: "https://fresenius-medical-2-47517.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
